d78fe251650d5e6bdd3b6598a42ef7f973080c8b
DanielTemoche <danielvela@upeu.edu.pe>
2020-09-21 10:49:16 -0500

    Initial commit

---
+++ app/src/main/java/com/kytom/frontbcp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kytom.frontbcp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
